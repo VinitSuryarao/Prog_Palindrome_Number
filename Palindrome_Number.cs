439eeb0c7c73385eb85155dcf819f9bb297b1d87
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:01:28 +0530

    Add files via upload

---
+++ Palindrome_Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Palindrome Number
+        //   If Number = 121 then Reverse also Same 121
+
+        static void Main(string[] args)
+        {
+            int n = 121, rev = 0, temp;
+            temp = n;
+            while(temp != 0)
+            {
+                rev = rev * 10;
+                rev = rev + temp % 10;
+                temp = temp / 10;
+            }
+
+            if ( n == rev)
+                Console.WriteLine("This is Palindrome");
+        }
+    }
+
+
